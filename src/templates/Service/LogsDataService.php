85e39917a8fd4559543a79d8cfb67126d2eb60d4
Adri <adrien.boitelle@gmail.com>
2024-01-20 09:37:55 +0100

    add logs

---
+++ src/templates/Service/LogsDataService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MyHordes\Prime\Service;
+
+use App\Entity\LogEntryTemplate;
+use MyHordes\Plugins\Interfaces\FixtureProcessorInterface;
+
+class LogsDataService implements FixtureProcessorInterface {
+
+    public function process(array &$data): void
+    {
+		$data = array_merge_recursive($data, [
+            ['text'=>'Les Hordes commencent à sérieusement s\'intéresser à vous... peut-être serait-il mieux de commencer à vous barricader.', 'name'=>'nightlyAttackStarting', 'type'=>LogEntryTemplate::TypeNightly, 'class'=>LogEntryTemplate::ClassCritical, 'secondaryType'=>null, 'variableTypes'=>array()],
+		]);
+    }
+}
