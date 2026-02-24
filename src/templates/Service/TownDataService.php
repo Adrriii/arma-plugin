62e26d91648e684022e81e70d7187a8ef2c7d886
Adri <adrien.boitelle@gmail.com>
2026-02-24 22:11:18 +0100

    add new town type

--- src/templates/Service/TownDataService.php
+++ src/templates/Service/TownDataService.php
@@ -8,13 +8,22 @@ class TownDataService implements FixtureProcessorInterface {
 
     public function process(array &$data, ?string $tag = null): void
     {
-        $data = array_replace_recursive($data, [
+        $order = 0;
+        $data = [
+            'arma' =>  [
+                'name' =>'arma',
+                'label' =>'Armageddon',
+                'preset' => true,
+                'ranked' => [1,7,15],
+                'orderBy' => $order--,
+                'help' => '<strong>Serez-vous assez fou </strong>? Si vous souhaitez vivre l\'expérience ultime de Hordes Armageddon, c\'est ici que ça se passe.',
+            ],
             'small' =>  [
                 'name' =>'small',
                 'label' =>'Ville rapide',
                 'preset' => true,
                 'ranked' => [1,15,35],
-                'orderBy' =>  2,
+                'orderBy' => $order--,
                 'help' => 'Vous incarnez un petit group de survivants d\'élite. 10 joueurs face à l\'Armageddon, mais qui en valent pas moins de 20. Ok, on est loin des 40... Bonne chance.',
             ],
             'remote' => [
@@ -22,24 +31,24 @@ class TownDataService implements FixtureProcessorInterface {
                 'label'=> 'Ville défi',
                 'preset' => true,
                 'ranked' => [1,10,25],
-                'orderBy' =>  1,
+                'orderBy' => $order--,
                 'help' => '20 survivants d\'élite, de quoi rivaliser avec les plus grandes légendes de l\'Outre-Monde.',
             ],
             'panda' =>  [
                 'name' =>'panda',
-                'label' =>'Armageddon',
+                'label' =>'Ville impossible',
                 'preset' => true,
                 'ranked' => [1,7,15],
-                'orderBy' => 0,
-                'help' => '<strong>Vous n\'êtes pas à la hauteur</strong>. Mais si vous souhaitez tout de même vivre l\'expérience ultime de Hordes Armageddon, c\'est ici que ça se passe.',
+                'orderBy' => $order--,
+                'help' => '<strong>Vous n\'êtes pas à la hauteur</strong>.',
             ],
             'custom' => [
                 'name'=>'custom',
                 'label'=>'Private Stadt',
                 'preset' => false,
                 'ranked' => false,
-                'orderBy' => -1
+                'orderBy' => $order--
             ],
-        ]);
+        ];
     }
 }
